77018f5a797868c0c2938591e5f9330c87e60743
Edgar Jr. San Martin <esanmartinjr@fordham.edu>
2018-05-23 20:30:56 -0400

    Update BestFit.py
    Added the functions squared_error and coefficient_of_determination, to calculate the coefficient of determination value

--- BestFit.py
+++ BestFit.py
@@ -10,6 +10,7 @@ from matplotlib import style
 
 style.use("fivethirtyeight")
 
+# Testing data
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,7,8,9,7], dtype=np.float64)
 
@@ -25,6 +26,15 @@ def best_fit_slope_and_intercept(xs, ys):
     b = mean(ys) - m*mean(xs)
     return m, b
 
+def squared_error(ys_orig, ys_line):
+    return sum(ys_line - ys_orig)**2
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr/ squared_error_y_mean)
+
 m,b = best_fit_slope_and_intercept(xs, ys)
 
 # creates regression line
@@ -34,6 +44,9 @@ regression_line = [(m*x)+b for x in xs]
 predict_x = 8
 predict_y = (m*predict_x)+b
 
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
 
 # generates scatter plot WITH the regression line
 plt.scatter(xs, ys)
