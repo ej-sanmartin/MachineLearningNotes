56d2197c7078c00c54dcdef1c61e2022ce0e5be7
Edgar Jr. San Martin <esanmartinjr@fordham.edu>
2018-05-25 02:53:24 -0400

    Update BestFit.py
    Added a create_dataset function, that randomly generates a dataset

--- BestFit.py
+++ BestFit.py
@@ -7,16 +7,31 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use("fivethirtyeight")
 
 # Testing data
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,7,8,9,7], dtype=np.float64)
+# xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+# ys = np.array([5,4,7,8,9,7], dtype=np.float64)
 
 # generates a scatter plot with the xs and ys as points
 # plt.scatter(xs, ys)
 # plt.show()
+
+# generates random dataset
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation=="pos":
+            val += step
+        elif correlation and correlation=="neg":
+            val -= step
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtape=np.float64)
 
 def best_fit_slope_and_intercept(xs, ys):
     # calculates slope
@@ -34,6 +49,9 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr/ squared_error_y_mean)
+
+# calling create_dataset to generate a random dataset
+xs, ys = create_dataset(40, 40, 2, correlation="pos")
 
 m,b = best_fit_slope_and_intercept(xs, ys)
 
